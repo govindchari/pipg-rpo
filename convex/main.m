f53f482d98557e63336d7620e070c9dc371972d2
govindchari <govindchari1@gmail.com>
2023-02-20 10:55:24 -0800

    Added double integrator test

--- convex/main.m
+++ convex/main.m
@@ -1,21 +1,38 @@
 clear;clc;close all
 
-% addpath 'convex'
 addpath pipg/
+addpath pipg/projections/
 addpath utils/
-params = params();
+addpath double_integrator/
 
-%% Solve with YALMIP/ECOS
-[X, U, solve_time, parse_time, solve_status] = ecos_trajopt(params);
+%% Rendezvous Problem
+% params = params();
+% 
+% % Solve with YALMIP/Gurobi
+% [X, U, solve_time, parse_time, solve_status] = ecos_trajopt(params);
+% 
+% fprintf("Solve Status: %s\n", solve_status)
+% fprintf("Parse Time: %4.1f ms\n", parse_time)
+% fprintf("Solve Time: %4.1f ms\n", solve_time)
+% 
+% plotall(X, U, params)
+% 
+% % Solve with PIPG
+% 
+% prob = problem(params);
+% [X, U] = pipg_solver(params, prob);
+% plotall(X', U', params)
 
+%% Double Integrator Test
+params = double_integrator_params();
+prob = problem(params);
+
+[X, U, solve_time, parse_time, solve_status] = double_integrator_test(params);
 fprintf("Solve Status: %s\n", solve_status)
 fprintf("Parse Time: %4.1f ms\n", parse_time)
 fprintf("Solve Time: %4.1f ms\n", solve_time)
+plot_double_integrator(X, U, params)
 
-plotall(X, U, params)
+[X_pipg, U_pipg] = pipg_solver(params, prob);
+plot_double_integrator(X_pipg', U_pipg, params)
 
-%% Solve with PIPG
-
-prob = problem(params);
-[X, U] = pipg_solver(params, prob);
-plotall(X', U', params)
