81cbe9f9f6c57d61175eead7e539c7c28b60b9d3
govindchari <govindchari1@gmail.com>
2023-04-20 16:49:35 -0700

    Cleanup

--- ptr/src/subproblem.jl
+++ ptr/src/subproblem.jl
@@ -22,16 +22,10 @@ function solveSubproblem!(p::ptr)
     # Min fuel rendezvous
     objective = (sumsquares(u) + p.wD * norm(Δ) + p.wDσ * norm(Δσ) + p.wnu * norm(nu, 1)) / p.wnu
 
-    # objective = (sumsquares(u) + p.wD * norm(Δ) + p.wnu * norm(nu, 1)) / p.wnu
-
     # Dynamics Constraint
     constraints = Constraint[
         x[:, k+1] == p.A[:, :, k] * x[:, k] + p.A[:, 4:6, k] * u[:, k] + p.S[:, k] * σ + p.z[:, k] + nu[(k-1)*p.nx+1:k*p.nx] for k in 1:p.K-1
     ]
-    # constraints = Constraint[
-    #     # x[:, k+1] == p.A[:, :, k] * x[:, k] + p.A[:, 4:6, k] * u[:, k] + nu[(k-1)*p.nx+1:k*p.nx] for k in 1:p.K-1
-    #     # x[:, k+1] == p.A[:, :, k] * x[:, k] + p.A[:, 4:6, k] * u[:, k] for k in 1:p.K-1
-    # ]
 
     # Boundary Conditions
     # x = [r v]
